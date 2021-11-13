00aff16396b8a930c181c190546a7a524af5773f
tenmasato <narito1015@icloud.com>
2021-11-13 12:38:53 +0000

    [Update]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def after_sign_in_path(resource)
+    post_images_path
+  end
+
+
   protected
 
   def configure_permitted_parameters
