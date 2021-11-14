774ac79c3896770b36a1db34ec81b96441a82a80
tenmasato <narito1015@icloud.com>
2021-11-14 07:47:29 +0000

    favorites

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-   resources :post_comments,only:[:create,:destroy]
+    resource :favorites, only:[:create,:destroy]
+
+  resources :post_comments,only:[:create,:destroy]
   end
 end
